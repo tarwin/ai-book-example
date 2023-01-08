13299cd15c88dff9492ec25db8a9ff3628f85b93
Tarwin Stroh-Spijer <tarwin@gmail.com>
2023-01-07 16:40:29 -0800

    feat: added colah as example

---
+++ book.js
@@ -0,0 +1,17 @@
+window.BOOK = {
+  title: 'Colah the Koala',
+  cover: {
+    title: 'Colah the Koala',
+    subtitle: 'Colah meets Kiki',
+    author: 'ChatGTP and @tarwin',
+    illustrator: 'SD, SD Midjourney, Dall-e and @tarwin',
+  },
+  pages: [
+    { images: ['1-1.webp'], textPosition: 'bottom', text: `Once upon a time in the land down under, there was a sleepy little Koala named Colah. All Colah wanted to do was climb up into her eucalyptus tree and take a nice, long nap.` },
+    { images: ['2-1.webp', '2-2.webp'], textPosition: 'bottom', text: `But every time she closed her eyes, she was kept awake by the loud, annoying laughter of a Kookaburra named Kiki.\n\n"Kiki, please stop that racket!" Colah begged. "I'm trying to sleep!"\n\nBut Kiki just laughed even louder. "Sleep, sleep, who needs sleep?" he cackled. "Let's go exploring instead!"` },
+    { images: ['3-1.webp'], textPosition: 'bottom', text: `Colah groaned and rubbed her eyes. She didn't want to explore, she just wanted to sleep. But Kiki wouldn't take no for an answer.\n\n"Come on, Colah, let's go swimming in the billabong!" Kiki suggested.` },
+    { images: ['3-2.webp'], textPosition: 'bottom', text: `"No, thank you," Colah said politely. "I prefer to nap in my tree."\n\nBut Kiki wasn't about to give up. He started jumping up and down on Colah's tree, making all the leaves shake and rustle.\n\n"Wake up, wake up, Colah!" Kiki sang. "There's a whole world out there to see!"` },
+    { images: ['4-1.webp'], textPosition: 'bottom', text: `Colah couldn't take it anymore. She climbed down from her tree and gave Kiki a stern look.\n\n"Kiki, I'm sorry, but I really need my rest," Colah said firmly. "I'm a Koala, and sleeping is what we do. So please, let me sleep in peace."\n\nKiki finally stopped laughing and looked at Colah with a sheepish grin. "Oh, I'm sorry, Colah," he said. "I didn't realize you were so tired. I'll let you sleep now."` },
+    { images: ['5-1.webp'], textPosition: 'bottom', text: `Colah smiled and climbed back up into her tree. She settled into her cozy eucalyptus leaves and closed her eyes.\n\nAnd finally, after all the noise and excitement, Colah was able to drift off into a peaceful, dreamy sleep. The end.` },
+  ]
+}
